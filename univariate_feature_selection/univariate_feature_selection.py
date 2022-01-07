cf3f0237bedf58d964f206d8c54d63565ecc401c
Bernard Adabankah <adabankah.com@gmail.com>
2022-01-07 01:55:28 +0000

    add files of univariate feature selection

--- univariate_feature_selection/univariate_feature_selection.py
+++ univariate_feature_selection/univariate_feature_selection.py
@@ -2,7 +2,10 @@ from sklearn.datasets import load_iris
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
 X, y = load_iris(return_X_y=True)
+
 print(X.shape)
+print(f'Number of columns before selecting 2 best features is {X.shape[1]}')
 
 X_new = SelectKBest(chi2, k=2).fit_transform(X, y)
 print(X_new.shape)
+print(f'Number of columns REMAINING after selecting 2 best features is {X_new.shape[1]}')
